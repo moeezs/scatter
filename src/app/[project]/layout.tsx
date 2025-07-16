5c0dee25d17e6978865742333420f65325eded2e
Abdul Moeez Shaikh <abdulmoeezshaikh@gmail.com>
2025-07-15 23:44:48 -0400

    shelf -> scatter and also some lint fixes

--- src/app/[project]/layout.tsx
+++ src/app/[project]/layout.tsx
@@ -1,26 +1,19 @@
-import type { Metadata } from "next";
 import { PROJECTS } from "./projects";
 
-type Props = {
-  params: { project: string }
-}
-
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
+export const metadata = ({ params }: { params: { project: string } }) => {
   const project = PROJECTS[params.project as keyof typeof PROJECTS];
-  
   if (!project) {
     return {
       title: "Project Not Found",
     };
   }
-
   return {
     title: project.name,
     icons: {
       icon: `${project.url.replace(/\/$/, '')}/favicon.ico`,
     },
   };
-}
+};
 
 export default function ProjectLayout({
   children,
